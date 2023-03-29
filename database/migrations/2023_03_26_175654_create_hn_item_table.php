fd2f7133500e733395ced8396a74f2e49301e270
maxzhang <373884384@qq.com>
2023-03-29 18:10:23 +0800

    调整导航类型默认值

--- database/migrations/2023_03_26_175654_create_hn_item_table.php
+++ database/migrations/2023_03_26_175654_create_hn_item_table.php
@@ -19,7 +19,7 @@ class CreateHnItemTable extends Migration
             $table->integer('cat')->comment('所属分类');
             $table->string('desc')->default('')->comment('简介');
             $table->string('desc_min')->default('')->comment('一句话简介');
-            $table->integer('type')->default('')->comment('类型，1网址2公众号/小程序');
+            $table->integer('type')->default(1)->comment('类型，1网址2公众号/小程序');
             $table->string('link')->default('')->comment('地址');
             $table->string('icon')->default('')->nullable()->comment('图标');
             $table->string('bak_link')->nullable()->comment('备用地址');
